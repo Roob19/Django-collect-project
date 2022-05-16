eda34b45a3f4c265d0f166064c613fdbb53f9cfb
ruben <rubendasilva@me.com>
2022-05-16 14:12:18 -0700

    details def added to main_app views

--- beercollector/beercollector/main_app/views.py
+++ beercollector/beercollector/main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import *
+from .models import The_beer
 
 
 # Create your views here.
@@ -13,3 +13,7 @@ def about(request):
 
 def beer_index(request):
     return render(request, 'beercollection/index.html', {'allbeers': beers_in_collection})
+
+def beer_details(request, beer_id):
+    beer = The_beer.objects.get(id=beer_id)
+    return render(request, 'beercollection/details.html', {'beer':beer})
