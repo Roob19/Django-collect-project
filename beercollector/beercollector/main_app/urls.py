3c8edec98accb1c179a57ed12f9296a4f0971b00
ruben <rubendasilva@me.com>
2022-05-17 17:03:15 -0700

    end of class

--- beercollector/beercollector/main_app/urls.py
+++ beercollector/beercollector/main_app/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('about/', views.about, name='about'),
     path('allbeers/', views.beer_index, name='index'),
     path('allbeers/<int:beer_id>', views.beer_details, name='details'),
+    path('allbeers/create/', views.BeerCreate.as_view(), name='beer_create'),
+    path('allbeers/<int:pk>/update/', views.BeerUpdate.as_view(), name='beer_update'),
+    path('allbeers/<int:pk>/delete/', views.BeerDelete.as_view(), name='beer_delete'),
 ]
--- beercollector/beercollector/main_app/views.py
+++ beercollector/beercollector/main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse
 from .models import The_beer
-
 
 # Create your views here.
 from django.http import HttpResponse
@@ -18,4 +19,22 @@ def beer_index(request):
 
 def beer_details(request, beer_id):
     beer = The_beer.objects.get(id=beer_id)
+    print('beer_details' , beer.id)
+    # import pdb; pdb.set_trace()
     return render(request, 'beercollection/details.html', {'beer':beer})
+
+class BeerCreate(CreateView):
+    model = The_beer
+    fields = '__all__'
+    def get_success_url(self, **kwargs):
+        return reverse('details', args=(self.object.id,))
+
+class BeerUpdate(UpdateView):
+    model = The_beer
+    fields = ['name', 'style', 'abv', 'url_site']
+    def get_success_url(self, **kwargs):
+        return reverse('details', args=(self.object.id,))
+
+class BeerDelete(DeleteView):
+    model = The_beer
+    success_url = '/allbeers/'
